c1cde8fdf46b4bd928fa752bafb717415163e48b
Mejkejs <iwanekmaksymilian@gmail.com>
2021-06-21 22:05:28 +0200

    Better Code figuring a draw to work

--- main.js
+++ main.js
@@ -1,39 +1,106 @@
+const Game = (function(){
+    const players = [];
+    const indexesOfOPositions = [];
+    const indexesOfXPositions = []; 
+    const winIndex = [
+        [0,4,8],[0,3,6],[0,1,2],[2,4,6],[2,5,8],[3,4,5],[6,7,8],[1,4,7]
+    ];
+    let moves =  1;
+    let winner = false;
+    // Dom elements 
+    const boxes = document.querySelectorAll(".box");
+    const para = document.querySelector("p");
+    const restartBtn = document.querySelector("button")
+    let currentDiv;
+    //Game functions
+    function startGame(){
+        displaySign();
+    }
+    function displaySign(){
+            boxes.forEach(function(box, boxIndex){
+                box.addEventListener("click", () =>{
+                    if(!winner){
+                        if(box.dataset.sign == ""){ 
+                            currentDiv = box;
+                            gameConditions(boxIndex);
+                        }
+                    }else{
+                        return false;
+                    }
+                })
+            })
 
+    }
+    function gameConditions(boxIndex){
+        if(moves % 2 === 0){
+            currentDiv.innerHTML = players[1].sign; // display O
+            para.textContent = "X's move";
+            indexesOfOPositions.push(boxIndex);
 
-(function() {
+        }
+        else if(moves % 2 !== 0){
+            currentDiv.innerHTML = players[0].sign; //display X
+            para.textContent = "O's move";
+            indexesOfXPositions.push(boxIndex);
+        }
+        checkWinner()
+        currentDiv.dataset.sign = 'filled';
+        moves++;
+    }
+    
+    function checkWinner(){
+        if(moves % 2 === 0){
+            for(let i in winIndex){
+                if(winIndex[i].every(el => indexesOfOPositions.includes(el)) === true){
+                    para.textContent = "O's win";
+                    winner = true;
+                }
+            }
+        }
+        else if(moves % 2 !== 0){
+            for(let i in winIndex){
+                if(winIndex[i].every(el => indexesOfXPositions.includes(el)) === true){
+                    para.textContent = "X's win";
+                    winner = true;
+                }
+            }
+        }
+    }
+    restartBtn.addEventListener("click", ()=>{
+            boxes.forEach(function(box){
+                box.dataset.sign = "";
+                box.textContent = "";
+                moves = 1;
+                indexesOfOPositions.splice(0, indexesOfOPositions.length);
+                indexesOfXPositions.splice(0, indexesOfXPositions.length);
+                para.textContent = "X's move";
+                winner = false;
+                currentDiv = undefined;
+            })
+    })
 
+    return{
+        startGame: startGame,
+        players: players,
+    }
+})();
 
-const Gameboard = {
-    gameboard: ["X","O","X","X","O","X","X","O","X"],
-    players: [],
-    moves: 1,
-    winNumbers:  [
-            [0,4,8],[0,3,6],[0,1,2],[2,4,6],[2,5,8],[3,4,5],[6,7,8],[1,4,7]
-    ],
-    init: function(){
-        this.domElements();
-        this.displaySign();
-    },
-    domElements: function(){
-        this.boxes = document.querySelectorAll(".box");
-        this.currentDiv;
-    },
-    displaySign: function(){
-        this.boxes.forEach(function(box){
-            box.addEventListener("click", () =>
-                box.innerHTML = "X"
-            )
-        })
-    },
-}
 const Player = (name, sign) =>{
-    return{name, sign}
+    return {name,sign}
 }
-const player1 = Player("maks", "X")
-const player2 = Player("king", "O")
-Gameboard.players.push(player1, player2)
-console.log(Gameboard.players)
-// let moves = 1;
+const player1 = Player("Xesar", "X");
+const player2= Player("Olek", "O");
+Game.players.push(player1, player2);
+Game.startGame();
+
+
+
+
+
+
+
+
+
 // let currentDiv;
 // let INDEX;
 // const winNumbers = [
@@ -53,9 +120,7 @@ console.log(Gameboard.players)
 // const Player = (name, sign) =>{
 //     const arrowFunction = ()=>{
 //         if(currentDiv.dataset.sign == ""){ 
-//             console.log("siema")
 //             if(moves % 2 == 0){
-//                 this.sign = "O"
 //                 paraMove.textContent = "X's move"
 //                 indexesOfo.push(INDEX);
 //                 for(let i = 0; i<winNumbers.length; i++){
@@ -102,7 +167,6 @@ console.log(Gameboard.players)
 // }
 // const xPlayer = Player("Maks", "X");
 // xPlayer.displaySign()
-})()
 
 // function CheckIfWin(){
 //     for(let i = 0; i<winNumbers.length; i++){
